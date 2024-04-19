f640027ccfb3b14af6b90044c35a5519a1b5b6e3
charmingruby <mrrubycanal3105@gmail.com>
2024-04-19 15:49:23 -0300

    feat: initial web project

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+
+export default function Home() {
+  return (
+    <div>
+      <p className="font-secondary text-2xl">Hello world</p>
+      <Button>hello</Button>
+    </div>
+  )
+}
